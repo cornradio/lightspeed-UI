a039178d4556c617b16ffa2b2c3f72dca8934703
Name <kasusaland@gmail.com>
2025-02-15 11:48:28 +0800

    1

--- Lightspeed-UI/Form1.Designer.cs
+++ Lightspeed-UI/Form1.Designer.cs
@@ -318,6 +318,7 @@
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "lightspeed-UI";
